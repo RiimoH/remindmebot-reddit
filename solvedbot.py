25aebc0e287202f655f4df1db32275659f0c809d
RiimoH <remo.herzog@hotmail.com>
2019-02-15 23:19:21 +0100

    finish

--- solvedbot.py
+++ solvedbot.py
@@ -2,11 +2,17 @@
 
 import praw
 import configparser
+import sqlite3
+import datetime
 from loguru import logger
+
+'''
+Solvedbot. A bot to notify interestees of reddit.com. Specially aimed at subreddits like r/tipofmytounge and r/whatisthisthing.
+'''
 
 # Initialize logging with loguru
 logger.add('log_{time}.log', rotation="5MB")
-logger.debug("Debugfile for solvedbot.py, created {date} {time}")
+logger.debug("Debugfile for solvedbot.py")
 
 
 # Read Configs from .cfg file
@@ -14,7 +20,7 @@ config = configparser.ConfigParser()
 config.read("solvedbot.cfg")
 cgf_reddit = config['Reddit']
 cgf_settings = config['Settings']
-cgf_sql = config['SQL']
+# cgf_sql = config['SQL']
 
 
 # Log into Reddit PRAW Agent.
@@ -25,23 +31,50 @@ reddit = praw.Reddit(client_id=cgf_reddit['client_id'],
                      username=cgf_reddit['username'])
 
 
-def search_keyword_comment(new, keyword):
-    # Iterating through submissions and searching for the keyword in replies.
-    list_of_comments = []
-    for submission in new:
-        for comment in submission.comments.list():
-            if keyword in comment.body:
-                logger.info(f'{keyword} found in comment: {comment}\tby User: {comment.author} \tin submission: {submission}')
-                list_of_comments.append(comment)
+class Connect:
+    """
+    DB connection class
+    """
+    def __init__(self):
+        self.connection = sqlite3.connect('solvedbot.db')
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS data(parent_id TEXT, comment_id TEXT, user TEXT, date TEXT)")
 
-    return list_of_comments
+    def add(self, parent_id, comment_id, user):
+        # Handles Database entries
 
-def reply_to_keyword(comments):
-    # Replies to all comments found to contain the Keyword.
-    for comment in comments:
+        date = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+        self.cursor.execute(
+            f"INSERT INTO data(parent_id, comment_id, user, date) VALUES(?,?,?,?)",
+            (parent_id, comment_id, user, date)
+        )
+        self.connection.commit()
+        return True
+
+    def fetch(self, parent_id):
+        # After a Solution has been found, this function fetches all users to notify from the database and returns them like [(parent_id, comment_id, users , date),]
+        self.cursor.execute('SELECT user FROM data WHERE parent_id = ?', (parent_id,))
+        data = self.cursor.fetchall()
+        users = {x[0] for x in data}
+        return users
+
+    def delete(self, parent_id):
+        # removes all entries from the database, containing the parent_id.
+        self.cursor.execute('DELETE FROM data WHERE parent_id = ?', (parent_id,))
+        return True
+
+
+class Handler:
+    '''
+    Handler Class, concerned about finding, replying, and saving Comments, containing the Keyword.
+    '''
+
+    def reply(self, comment):
+        # Replies to the comment input.
         answer = f"""::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-Your notification was registered! We will notify you when either keyword "Found!" or "Solved!" are mentioned.
+Your notification was registered! We will notify you when the keyword "Found!" is mentioned.
 
 If you want to be notified too, please use the link below!
 
@@ -49,22 +82,61 @@ If you want to be notified too, please use the link below!
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"""
         comment.reply(answer)
-    return True
+        return True
+
+    def sendMessage(self, parent_id, users):
+        # Notifies all Notifees that a Solution has been found.
+        for user in users:
+
+            body = f"""::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+Hello {user}
+
+You wanted to be notified, as soon as a Solution is found.
+
+Good News, now it is! Check out the link below.
+
+[Link](http://www.reddit.com/{parent_id}/)
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"""
+
+            reddit.redditor(user).message(subject='SolvedBot Notification', message=body)
+        return True
 
 
 # Main function, logged with loguru.
 @logger.catch
 def main(reddit, cgf_settings):
-    
-    # Fetching submissions by new, with limit of 5.
-    subreddit = reddit.subreddit(cgf_settings['list_of_subreddits'])
-    new = subreddit.new(limit=5)
+    # Instanciating
+    handler = Handler()
+    database = Connect()
 
-    list_of_comments = search_keyword_comment(new, cgf_settings['keyword_searched_for'])
-    reply_to_keyword(list_of_comments)
+    # Fetching variables
+    keyword_searching = cgf_settings['keyword_searching']
+    keyword_found = cgf_settings['keyword_found']
+    subreddit = reddit.subreddit(cgf_settings['subreddit'])
 
-    logger.info('Programm ended. No issues.')
+    # Main Loop
+    try:
+        while True:
+            for comment in subreddit.stream.comments():
+                if comment.author == 'solvednotification':
+                    continue
+                else:
+                    text = str(comment.body)
+                    parent_id = str(comment.submission)
+                    comment_id = str(comment)
+                    author = str(comment.author)
 
+                    if keyword_searching in text:
+                        database.add(parent_id, comment_id, author)
+                        handler.reply(comment)
+                    elif keyword_found in text:
+                        users = database.fetch(parent_id)
+                        handler.sendMessage(parent_id, users)
+                        database.delete(parent_id)
+    except KeyboardInterrupt:
+        logger.info('Bot terminated. User input.')
 
 
 # Run main
